2013e329b594b1a7e86d4954df260b2124c27c38
wilsoncip <89822808+wilsoncip@users.noreply.github.com>
2021-12-08 10:05:18 -0500

    uodate

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     print("Hello")
+
+@app.route("/new_entry")
+def new_entry():
+    """
+    """
+
+@app.route("/show_all")
+def show_all():
+    """
+    """
--- tracker.py
+++ tracker.py
@@ -1,15 +1,15 @@
-import database as d
+import sqlite3 as sql
 
 def create_new_table(name):
     """creates a new table entry in the database, 
     if the table exists delete it and create a new one"""
-    if d.check_table(name) == 1:
-        d.delete_table(name)
+    if sql.check_table(name) == 1:
+        sql.delete_table(name)
     else:
-        d.create_table(name)
+        sql.create_table(name)
         
 
 def add_entry(name, amount, description):
-    d.insert_data(name, amount, description)
-    
+    sql.insert_data(name, amount, description)
+
 
