37b764fbab5b791eeab694eb4c7f68defcc2ce16
wilsoncip <89822808+wilsoncip@users.noreply.github.com>
2021-12-08 02:10:08 -0500

    postgres attempt and app

--- tracker.py
+++ tracker.py
@@ -1,6 +1,15 @@
-import database
+import database as d
 
-def create_database():
-    """Creates a new database
-    """
+def create_new_table(name):
+    """creates a new table entry in the database, 
+    if the table exists delete it and create a new one"""
+    if d.check_table(name) == 1:
+        d.delete_table(name)
+    else:
+        d.create_table(name)
+        
+
+def add_entry(name, amount, description):
+    d.insert_data(name, amount, description)
     
+
