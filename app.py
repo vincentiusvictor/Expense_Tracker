984a3f7e556636edbe3a1c6f719013682ba61b34
vincentiusvictor <vvictor1@babson.edu>
2021-12-08 19:04:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    print("Hello")
+def index():
+    return render_template("main_page.html")
 
 @app.route("/new_entry")
 def new_entry():
+    """add a new expesne. redirect to previous and necct page
     """
-    """
+    pass
+###Post and get
 
 @app.route("/show_all")
 def show_all():
+    """ redirect to new entry and main page 
     """
-    """
+    pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
