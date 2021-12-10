14eceb13edde552baded35b178c2b34441badbd2
vincentiusvictor <vvictor1@babson.edu>
2021-12-10 04:03:35 -0500

    Create line_chart.py

---
+++ line_chart.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from data import pull_table
+
+def time_plot(expenses):
+    time = []
+    for item in expenses:
+        time.append(item[0])
+    values = []
+    for item in expenses:
+        values.append(item[1])
+    print(time)
+    plt.plot(time,values)
+    plt.title('Expenses during the time')
+    plt.xlabel('Time')
+    plt.ylabel('Total expense ($)')
+    # plt.show()
+    plt.savefig("output2.jpg")
+
+data = pull_table("expenses")
+time_plot(data)
