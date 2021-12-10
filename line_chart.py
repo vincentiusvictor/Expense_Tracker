d38c154fba704ed7db9514403e1cc7b3f1d41110
vincentiusvictor <vvictor1@babson.edu>
2021-12-10 11:55:05 -0500

    Update line_chart.py

--- line_chart.py
+++ line_chart.py
@@ -1,22 +1,6 @@
 import matplotlib.pyplot as plt
 from data import pull_table
 
-# def time_plot(expenses):
-#     time = []
-#     for item in expenses:
-#         time.append(item[0])
-#     values = []
-#     for item in expenses:
-#         values.append(item[1])
-#     print(time)
-#     plt.plot(time,values)
-#     plt.title('Expenses during the time')
-#     plt.xlabel('Time')
-#     plt.ylabel('Total expense ($)')
-#     # plt.show()
-#     plt.savefig("templates/output2.jpg")
-#     plt.close()
-
 def filt(expenses):
     lst = []
     for item in expenses:
