b3333bac767f8df31bd5ad60b5e988931f2d7d8a
vincentiusvictor <vvictor1@babson.edu>
2021-12-10 00:18:07 -0500

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -1,5 +1,6 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+from postgresql import insert_data, pull_table
 
 def bar_graph(expenses):
     cat = list(expenses.keys())
@@ -11,7 +12,48 @@ def bar_graph(expenses):
     plt.title("Total amount of $ of expense per category ")
     plt.show()
 
-
-
 data = {"Home & Utilities":100, "Transportation":200}
-bar_graph(data)
+# bar_graph(data)
+
+insert_data("payments", 500, 'lunch')
+insert_data("payments", 200, 'dinner')
+a = pull_table("payments")
+print(a)
+
+def Convert(lst):
+    res_dct = {lst[i+1]: lst[i] for i in range(0, len(lst), 2)}
+    return res_dct
+
+def filter(x):
+    for item in x:
+        print(list(item[1:3]))
+
+
+
+# for item in a:
+#     a, b =list(item[1:3])
+# print(a)
+# print(b)
+# for item in a:
+#     print(item[1:3].split('\n'))
+
+# def one_list(b):
+#     list_of_lists = []
+#     for line in b:
+#         stripped_line = line.strip()
+#         line_list = stripped_line.split()
+#         list_of_lists.append(line_list)
+#         b.close()
+#         print(list_of_lists)
+
+# for item in a:
+#     print(one_list(item[1:3]))
+
+
+
+# print(Convert(pull_table("payments")))
+
+# dates = ['2015-12-20','2015-09-12']  
+# PM_25 = [80, 55]
+# data = pd.DataFrame({'dates':pd.to_datetime(dates),'PM_25':PM_25})
+# data.plot(x='dates',y='PM_25',marker='o',linestyle='none',color='red',ms=3)
