400036a88d7b9d75ea878697a775ca9e2e72b44b
wilsoncip <89822808+wilsoncip@users.noreply.github.com>
2021-12-10 20:57:20 -0500

    Update table.py

--- table.py
+++ table.py
@@ -1,12 +1,17 @@
 from flask_table import Table, Col
 
 class ItemTable(Table):
+    """Creates a class that contains all the data for the table.
+    Contains: date, amount, category, description
+    """
     date = Col('Date')
     amount = Col('Amount')
     category = Col('Category')
     description = Col('Description')
 
 class Item(object):
+    """Creates a class that contains one entry worht of data.
+    """
     def __init__(self, date, amount, category, description, border=True):
         self.date = date
         self.amount = amount
@@ -14,6 +19,8 @@ class Item(object):
         self.description = description
 
 def create_items(data):
+    """takes in raw data from SQL and coverts it into an Item object.
+    """
     lst = []
     for item in data:
         entry = Item(item[0], item[1], item[2], item[3])
